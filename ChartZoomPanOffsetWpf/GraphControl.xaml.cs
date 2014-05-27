eb69b907783bfd0f22ce6b5d890a88c8fe171e8f
Elena Shauliuk <elena.shauliuk@bdcsoft.com>
2014-05-27 17:40:02 -0500

    init

---
+++ ChartZoomPanOffsetWpf/GraphControl.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using Telerik.Windows.Controls;
+
+namespace ChartZoomPanOffsetWpf {
+    /// <summary>
+    /// Interaction logic for GraphControl.xaml
+    /// </summary>
+    public partial class GraphControl : UserControl {
+        public GraphControl() {
+            InitializeComponent();
+        }
+    }
+    
+    public class ChartData {
+        public DateTime Date { get; set; }
+
+        public double Value { get; set; }
+    }
+
+
+    public class GraphViewModel : ViewModelBase {
+        private readonly Random rand = new Random(123456);
+
+        public GraphViewModel() {
+            Data = GetData();
+        }
+
+        public List<ChartData> Data { get; set; }
+
+        private List<ChartData> GetData() {
+            var data = new List<ChartData>();
+            for (int i = 0; i < 200; i++) {
+                data.Add(new ChartData() {Date = DateTime.Today.AddDays(i), Value = rand.Next(10, 100)});
+            }
+
+            return data;
+        }
+
+        private Point _panOffset;
+        public Point PanOffset {
+            get {
+                return _panOffset;
+            }
+            set {
+                if (this._panOffset != value) {
+                    this._panOffset = value;
+                    this.OnPropertyChanged("PanOffset");
+                }
+            }
+        }
+
+        private Size _zoom;
+        public Size Zoom {
+            get {
+                return _zoom;
+            }
+            set {
+                if (this._zoom != value) {
+                    this._zoom = value;
+                    this.OnPropertyChanged("Zoom");
+                }
+            }
+        }
+    }
+
+}
---
+++ ChartZoomPanOffsetWpf/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using Telerik.Windows.Controls;
+
+namespace ChartZoomPanOffsetWpf {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void RadTabControlBase_OnSelectionChanged(object sender, RadSelectionChangedEventArgs e) {
+            var shell = DataContext as ViewModel;
+            if (shell != null) {
+                if (Tab1.IsSelected) {
+                   // shell.Graph1Control.RadChart1.Zoom = shell.Graph2Control.RadChart1.Zoom;
+                   // shell.Graph1Control.RadChart1.PanOffset = shell.Graph2Control.RadChart1.PanOffset;
+                    ((GraphViewModel)shell.Graph1Control.DataContext).Zoom = ((GraphViewModel)shell.Graph2Control.DataContext).Zoom;
+                    ((GraphViewModel)shell.Graph1Control.DataContext).PanOffset = ((GraphViewModel)shell.Graph2Control.DataContext).PanOffset;
+                }
+                else if (Tab2.IsSelected) {
+                   // shell.Graph2Control.RadChart1.Zoom = shell.Graph1Control.RadChart1.Zoom;
+                   // shell.Graph2Control.RadChart1.PanOffset = shell.Graph1Control.RadChart1.PanOffset;
+                    ((GraphViewModel)shell.Graph2Control.DataContext).Zoom = ((GraphViewModel)shell.Graph1Control.DataContext).Zoom;
+                    ((GraphViewModel)shell.Graph2Control.DataContext).PanOffset = ((GraphViewModel)shell.Graph1Control.DataContext).PanOffset;
+                }
+            }
+        }
+    }
+    
+
+    public class ViewModel : ViewModelBase {
+        public ViewModel() {
+            Graph1Control = new GraphControl();
+            Graph2Control = new GraphControl();
+        }
+
+        public GraphControl Graph1Control { get; set; }
+        public GraphControl Graph2Control { get; set; }
+    }
+}
